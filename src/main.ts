b80ee81b337ffdf4c593fc287551e2e7399c0bf2
Paul <github@witte-steur.nl>
2026-07-24 12:41:45 +0200

    Set up app structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+import './screen.css';
+
+createApp(App).mount('#vue-app');
