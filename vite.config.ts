6bd686112374f62a5b0c49e4524242c6b4616279
Paul <github@witte-steur.nl>
2026-07-24 03:59:30 +0200

    Configured Vite development server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    port: 8000,
+  },
+});
